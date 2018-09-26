f2ca87090fd85a118a8c11d4854d808c4006ad83
Eliot Waters <waterse1080@my.uwstout.edu>
2018-09-26 12:07:54 -0500

    minor edits

--- src/states/MainMenu.js
+++ src/states/MainMenu.js
@@ -5,9 +5,7 @@ import Phaser from 'phaser'
 import config from '../config'
 
 class MainMenu extends Phaser.State {
-
   init () {
     this.game.world.setBounds(0, 0, this.game.width, this.game.height)
   }
-
 }
