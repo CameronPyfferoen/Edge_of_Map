26a09ad017de779dd17ad0104721530d002973f5
Eliot Waters <waterse1080@my.uwstout.edu>
2018-09-26 10:04:13 -0500

    Fixed indentation in main menu

--- src/states/MainMenu.js
+++ src/states/MainMenu.js
@@ -4,9 +4,9 @@ import Phaser from 'phaser'
 // Import config settings
 import config from '../config'
 
-class MainMenu extends Phaser.State{
- 
-  init(){
+class MainMenu extends Phaser.State {
+
+  init () {
     this.game.world.setBounds(0, 0, this.game.width, this.game.height)
   }
 
