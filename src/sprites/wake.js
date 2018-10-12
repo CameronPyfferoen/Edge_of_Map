df6024a233745f0887fb9ebd81feafdec4780223
Eliot Waters <waterse1080@my.uwstout.edu>
2018-10-12 13:08:26 -0500

    changed wake to be from a phaser emitter

--- src/sprites/wake.js
+++ src/sprites/wake.js
@@ -6,12 +6,12 @@ class Wake extends Phaser.Sprite {
     super(game, x, y, 'wake', 0)
     this.name = 'wake'
     this.anchor.setTo(0.5, 0.5)
-    this.timer = 0;
+    this.timer = 0
     this.game = game
 
     this._SCALE = config.PLAYER_SCALE
     this.scale.setTo(this._SCALE)
-
+    /*
     this.game.physics.p2.enable(this)
 
     this.body.debug = __DEV__
@@ -22,14 +22,15 @@ class Wake extends Phaser.Sprite {
 
     this.body.damping = 0.5
     this.body.data.gravityScale = 0
+    */
     this.setupAnimations()
     this.animations.play('idle', true)
   }
 
   update () {
     super.update()
-    
-    this.timer += 1;
+
+    this.timer += 1
     if (this.timer > 10) {
       console.log('destroyed')
       this.destroy()
