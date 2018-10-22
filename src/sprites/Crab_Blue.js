db4f31678ebeebe4bbc30e8c263d546382caeb31
CameronPyfferoen <pyfferoenc3608@my.uwstout.edu>
2018-10-22 12:57:30 -0500

    Added classes and images

---
+++ src/sprites/Crab_Blue.js
@@ -0,0 +1,14 @@
+import Enemy from './Enemy'
+
+class Crab_Blue extends Enemy
+{
+  constructor ( game ) {
+    super ( game )
+    this.setupAnimations()
+  }
+
+  setupAnimations()
+  {
+    this.setupAnimations.add('swim', )
+  }
+}
