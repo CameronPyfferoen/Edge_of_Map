ceaa9d2b92a18226bbe6c2a81488f88447388a10
Eliot Waters <waterse1080@my.uwstout.edu>
2018-10-26 13:17:47 -0500

    tiled testing

---
+++ assets/maps/Island_3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Island_3" tilewidth="32" tileheight="32" tilecount="16" columns="4">
+ <image source="../images/T_I_3.png" width="138" height="139"/>
+</tileset>
