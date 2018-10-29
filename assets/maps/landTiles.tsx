da0fc4e10349e2b13eb5a63045ef0c41312128c2
Eliot Waters <waterse1080@my.uwstout.edu>
2018-10-29 13:11:51 -0500

    updated map

---
+++ assets/maps/landTiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="landTiles" tilewidth="32" tileheight="32" tilecount="6076" columns="98">
+ <image source="../images/inprogress_map_template_no_back.png" width="3149" height="2007"/>
+</tileset>
