eb222bc8e212dda3d636dfadba165bc18ec96c61
Seth Berrier <berriers@uwstout.edu>
2018-12-23 12:27:13 -0600

    Tweek to Windows Installer Script

--- buildWinInstaller.sh
+++ buildWinInstaller.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 # Customize for specific game
-dirName=edge-of-the-map
+dirName="edge-of-the-map"
 pkgName="Edge of the Map"
 
 # Move to dir and rename it
 cd "./packaged/${dirName}/"
 mv win64 "${pkgName}"
 
+# Delete previous archive
+test "../${pkgName}-win64.zip" & rm "../${pkgName}-win64.zip"
+
 # Create archive
-zip -r "../${pkgName}-win64.zip" "./${pkgName}"
+zip -r9 "../${pkgName}-win64.zip" "./${pkgName}"
 
 # Name back and move back
 mv "${pkgName}" win64
